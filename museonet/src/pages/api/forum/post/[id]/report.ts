219f0ba54939cd21b63d29a6940b1add16894544
Zhakeshe <zhakezh3@gmail.com>
2026-01-29 12:17:41 +0600

    Use alias import in forum post report API

--- museonet/src/pages/api/forum/post/[id]/report.ts
+++ museonet/src/pages/api/forum/post/[id]/report.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
-import { reportForumPost } from '../../../../lib/db';
+import { reportForumPost } from '@/lib/db';
 
 const payloadSchema = z.object({
   reporterEmail: z.string().email(),
